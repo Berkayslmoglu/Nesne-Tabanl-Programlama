9ac3b90e1fc2c3ca59ba80c63b3a2eb7a0a1ddd4
Berkayslmoglu <145571503+Berkayslmoglu@users.noreply.github.com>
2024-06-04 18:46:58 +0300

    Add files via upload

---
+++ ClassUygulama/ClassUygulama/Dikdortgen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassUygulama
+{
+    class Dikdortgen
+    {
+        private int a, b;
+        
+        public Dikdortgen(int a, int b)//Parametreli Metot.
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
