48fd12e606f7cdd93aeb5a61da370202853fe5e0
Berkayslmoglu <145571503+Berkayslmoglu@users.noreply.github.com>
2024-06-04 18:51:42 +0300

    Add files via upload

---
+++ Metot_Buyuk_Sayi_Bulma/Metot_Buyuk_Sayi_Bulma/BuyukSayiBul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot_Buyuk_Sayi_Bulma
+{
+    class BuyukSayiBul
+    {
+        public int BuyukOlanibul(int sayi1, int sayi2)
+        {
+            int sonuc;
+            if (sayi1 < sayi2)
+            {
+                sonuc = sayi2;
+            }
+            else
+            {
+                sonuc = sayi1;
+            }
+            return sonuc;
+        }
+    }
+}
---
+++ Metot_Buyuk_Sayi_Bulma/Metot_Buyuk_Sayi_Bulma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot_Buyuk_Sayi_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuyukSayiBul bs = new BuyukSayiBul();
+            int a = 100;
+            int b = 25;
+            int sonuc = bs.BuyukOlanibul(a, b);
+            Console.WriteLine("Büyük olan sayı: {0}", sonuc);
+            Console.ReadLine();
+        }
+    }
+}
