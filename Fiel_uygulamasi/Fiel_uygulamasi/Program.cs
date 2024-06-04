f3c4bbd494e576d0ff3b25be242fd2bee7187c56
Berkayslmoglu <145571503+Berkayslmoglu@users.noreply.github.com>
2024-06-04 18:49:32 +0300

    Add files via upload

---
+++ Fiel_uygulamasi/Fiel_uygulamasi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiel_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucgen ucgen = new Ucgen();
+            Console.WriteLine("A değeri gir: ");
+            ucgen.A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("B değeri gir: ");
+            ucgen.B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("C değeri gir: ");
+            ucgen.C = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A kenarının Uzunluğu: " + ucgen.A);
+            Console.WriteLine("B kenarının Uzunluğu: " + ucgen.B);+
+            Console.WriteLine("C kenarının Uzunluğu: " + ucgen.C);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Fiel_uygulamasi/Fiel_uygulamasi/Ucgen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiel_uygulamasi
+{
+/* Field:Bir Nesnenin Özelliklerini Veya Veri Alanlarını(Elemanlarını) Temsil Eden Değişkenlerderdir. */
+    class Ucgen
+    {
+        //Field
+        public int a;
+        public int b;
+        public int c;
+/* Get: Almak, elde etmek, getirmek anlamında gelir
+ *  bir değer döndürmek için kullanılır(return ile).
+ *  Get: Get özelliği Kullanılırsa bu özelliği değer ataması yapılamaz. Bu özellik sadece okunanilir(Readonly) özellil olur.
+ *  Set: Düzenlemek, ayarlamak, anlamına gelir.
+ *  Değişkene değer atamak için kullanılır.
+ *  Set ifadesi değer döndürmez, sadece değer alabilir. */
+        public int A
+        {
+            get 
+            {
+                return a;
+            }
+            set
+            {
+                if(value<=0)
+                {
+                    Console.WriteLine("Hatalı Bilgi");
+                }
+                else
+                {
+                    a = value;
+                }
+            }
+        }
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Hatalı Bilgi");
+                }
+                else
+                {
+                    b = value;
+                }
+            }
+        }
+        public int C
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Hatalı Bilgi");
+                }
+                else
+                {
+                    c = value;
+                }
+            }
+        }
+    }
+}
