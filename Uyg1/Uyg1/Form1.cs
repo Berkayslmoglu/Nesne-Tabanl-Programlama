da7936ccda79e13cc658f325a54e5cd8638e2ce4
Berkayslmoglu <145571503+Berkayslmoglu@users.noreply.github.com>
2024-06-04 18:57:44 +0300

    Add files via upload

---
+++ Uyg1/Uyg1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uyg1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            while(i < 10);
+            {
+                i++;
+                if(i==5)
+                {
+                    MessageBox.Show("CONTİNUE");
+                    continue;
+                }
+            }
+            MessageBox.Show("i :" +i);
+            if(i==9)
+            {
+                MessageBox.Show("BREAK");
+                break;
+            }
+        }
+    }
+}
